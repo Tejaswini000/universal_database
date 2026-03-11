9354fd2581461dd60e150fa7b1867703b6ce1fc5
Tejaswini000 <141923774+Tejaswini000@users.noreply.github.com>
2026-03-11 02:59:35 -0400

    feat: add user database check

--- number_guess.sh
+++ number_guess.sh
@@ -6,6 +6,16 @@ SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
 
 echo "Enter your username:"
 read USERNAME
-Y
-#!/bin/bash
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME';")
 
+if [[ -z $USER_ID ]]
+then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME');")
+else
+  GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games WHERE user_id=$USER_ID;")
+  BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id=$USER_ID;")
+
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+
+fi
