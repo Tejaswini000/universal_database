1d46f8c5d10b769b0a4d5afb77d596030ac17486
Tejaswini000 <141923774+Tejaswini000@users.noreply.github.com>
2026-03-11 02:55:40 -0400

    feat: add username input and random number

--- number_guess.sh
+++ number_guess.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+
+echo "Enter your username:"
+read USERNAME
+Y
+#!/bin/bash
+
